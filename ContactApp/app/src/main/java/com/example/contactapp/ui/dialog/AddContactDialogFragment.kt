fc2331386e5f98fa08bfb9bd6a745fd3570ddae2
Ethan <fanfirst4@naver.com>
2023-09-05 14:27:50 +0900

    [feature] dialogbtn color 변경

--- ContactApp/app/src/main/java/com/example/contactapp/ui/dialog/AddContactDialogFragment.kt
+++ ContactApp/app/src/main/java/com/example/contactapp/ui/dialog/AddContactDialogFragment.kt
@@ -1,9 +1,11 @@
 package com.example.contactapp.ui.dialog
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.DialogFragment
 import com.example.contactapp.R
 import com.example.contactapp.databinding.FragmentAddContactDialogBinding
@@ -13,6 +15,7 @@ import com.example.contactapp.manager.ContactManagerImpl
 class AddContactDialogFragment : DialogFragment() {
 
     private lateinit var binding: FragmentAddContactDialogBinding
+    private var alarmNumber: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,8 +46,36 @@ class AddContactDialogFragment : DialogFragment() {
             contactManagerImpl.createContact(name, phoneNumber, email, alarm)
 
             dismiss()
-
         }
+
+        binding.offbtn.setOnClickListener {
+            setButtonState(binding.offbtn)
+            alarmNumber = 0
+        }
+
+        binding.tenBtn.setOnClickListener {
+            setButtonState(binding.tenBtn)
+            alarmNumber = 1
+        }
+
+        binding.fivebtn.setOnClickListener {
+            setButtonState(binding.fivebtn)
+            alarmNumber = 2
+        }
+
+        binding.thirtyBtn.setOnClickListener {
+            setButtonState(binding.thirtyBtn)
+            alarmNumber = 3
+        }
+    }
+
+    private fun setButtonState(selectedButton: Button) {
+        binding.offbtn.setBackgroundColor(Color.parseColor("#D8BFD8"))
+        binding.fivebtn.setBackgroundColor(Color.parseColor("#D8BFD8"))
+        binding.tenBtn.setBackgroundColor(Color.parseColor("#D8BFD8"))
+        binding.thirtyBtn.setBackgroundColor(Color.parseColor("#D8BFD8"))
+
+        selectedButton.setBackgroundColor(Color.parseColor("#E4DA76D5"))
     }
 
     // dialog size 조절
